c58ea258bbc8fa2f665fbabae9930b6ecdf90520
imarkwick <izzymarkwick@gmail.com>
2015-05-01 14:25:31 +0100

    adding table numbers and names to the database

--- routes/index.js
+++ routes/index.js
@@ -17,4 +17,31 @@ router.get('/volunteer', function(req, res) {
 	})
 });
 
+/* GET admin page for adding to table names */
+router.get('/admin', function(req, res) {
+	res.render('admin', {
+		title: 'Add guests to a table'
+	})
+});
+
+/* POST to add guest */
+router.post('/addguest', function(req, res) {
+	var db = req.db;
+	var tableNumber = req.body.tableno;
+	var guests = req.body.names;
+	var collection = db.get('tablecollection');
+
+	collection.insert({
+		"tableno" : tableNumber,
+		"names" : guests
+	}, function(err, doc) {
+		if (err) {
+			res.send("There was a problem add information to the database");
+		} else {
+			res.location("volunteer");
+			res.redirect("volunteer");
+		}
+	});
+});
+
 module.exports = router;
