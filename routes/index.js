9607738839607eb32ce1b5fb375951abaae890af
imarkwick <izzymarkwick@gmail.com>
2015-05-07 21:32:31 +0100

    display is ONLY socket messages and background image

--- routes/index.js
+++ routes/index.js
@@ -100,31 +100,32 @@ router.post('/add-to-display', function(req, res) {
 
 /* GET display items */
 router.get('/display', function(req, res) {
-	var db = req.db;
-	var collection = db.get('itemcollection');
-	var itemArray = []
-	var items = []
-	collection.find({},{},function(e,docs) {
-		var data = docs
-		console.log(data);
-		String.prototype.repeat = function(num) {
-	    return new Array(num + 1).join(this + ',');
-	  };		
-		data.forEach(function(item) {
-			if (typeof item.names === 'string')
-				item.names = [item.names];
-			itemArray.push( (item.item).repeat(item.names.length) );
-		}) 
-		itemArray.forEach(function(string) {
-			var last = string.length-1;
-			items.push(string.slice(0, last));
-		});
-		var images = items.join().split(',');
-		res.render('display', {
-			itemlist : docs,
-			images : images
-		});
-	});
+	res.render('display');
+	// var db = req.db;
+	// var collection = db.get('itemcollection');
+	// var itemArray = []
+	// var items = []
+	// collection.find({},{},function(e,docs) {
+	// 	var data = docs
+	// 	console.log(data);
+	// 	String.prototype.repeat = function(num) {
+	//     return new Array(num + 1).join(this + ',');
+	//   };		
+	// 	data.forEach(function(item) {
+	// 		if (typeof item.names === 'string')
+	// 			item.names = [item.names];
+	// 		itemArray.push( (item.item).repeat(item.names.length) );
+	// 	}) 
+	// 	itemArray.forEach(function(string) {
+	// 		var last = string.length-1;
+	// 		items.push(string.slice(0, last));
+	// 	});
+	// 	var images = items.join().split(',');
+	// 	res.render('display', {
+	// 		itemlist : docs,
+	// 		images : images
+	// 	});
+	// });
 });
 
 module.exports = router;
