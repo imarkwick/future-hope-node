f6d250acda4ca498fa1fbdce70eeeb93569cad38
imarkwick <izzymarkwick@gmail.com>
2015-05-11 15:02:33 +0100

    unable to visit volunteer page unless you have a session/table number

--- controllers/volunteer.js
+++ controllers/volunteer.js
@@ -4,17 +4,21 @@ module.exports.controller = function(app, tables) {
 	app.get('/volunteer', function(req, res) {
 		var db = req.db;
 		var collection = db.get('tablecollection')
-		req.session.reload(function(err) {
-			if(err) {
-				console.log(err);
-			} else {
-				var mynumber = req.session.table;
-				collection.find({ tableno: mynumber }, function(e, docs){
-					var guests = tables.getNames(docs);
-					res.render('volunteer', { number : mynumber, guests : guests });
-				});
-			}	
-		});
+		if (req.session.table) {
+			req.session.reload(function(err) {
+				if(err) {
+					console.log(err);
+				} else {
+					var mynumber = req.session.table;
+					collection.find({ tableno: mynumber }, function(e, docs){
+						var guests = tables.getNames(docs);
+						res.render('volunteer', { number : mynumber, guests : guests });
+					});
+				}
+			});
+		} else {
+				res.redirect('/volunteer-table');
+		};
 	});
 
 	/* POST to add an auction item */
