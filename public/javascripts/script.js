bf06b4fd6a3267dbbe33dbf200738b32849e7b57
clare trembath <claretrembath@ymail.com>
2015-05-12 15:50:34 +0100

    thankyou

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -21,5 +21,8 @@ $(document).ready(function() {
   String.prototype.repeat = function(num) {
     return new Array(num + 1).join(this + ',');
   };
+  function randomFrom(array) {
+            return array[Math.floor(Math.random() * array.length)];
+          }
 });
 
