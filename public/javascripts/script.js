c8338a37b98ffc15b22a563efd192065e7a4c455
imarkwick <izzymarkwick@gmail.com>
2015-05-13 13:00:57 +0100

    showing names on the display

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -21,8 +21,6 @@ $(document).ready(function() {
   String.prototype.repeat = function(num) {
     return new Array(num + 1).join(this + ',');
   };
-  function randomFrom(array) {
-            return array[Math.floor(Math.random() * array.length)];
-          }
+
 });
 
