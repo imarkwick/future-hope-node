df413b693f39d0223e9555aa05660854e45348db
imarkwick <izzymarkwick@gmail.com>
2015-05-19 13:00:54 +0100

    error msg if auction item is selected as name of the item

--- public/javascripts/volunteer.js
+++ public/javascripts/volunteer.js
@@ -2,34 +2,40 @@ $(document).ready(function() {
 
   var socket = io();
 
-  $('#mela-auction').submit(function() {
-  	var item = $('#mela-item').val();
-  	var names = $('#guest-list').val();
-  	var total = Object.keys(names).length;
-  	$('#confirmation').show();
-  });
+  // $('#mela-auction').submit(function(e) {
+  // 	e.preventDefault();
+  // 	var item = $('#mela-item').val();
+  // 	var names = $('#guest-list').val();
+  // 	var total = Object.keys(names).length;
+  // 	$('#confirmation').show();
+  // });
 
   $('#mela-auction').submit(function(e) {
   	e.preventDefault();
   	var item = $('#mela-item').val();
   	var names = $('#guest-list').val();
   	if (item && names) {
-  		$.ajax({
-	  		type: "POST",
-	  		url: "/add-to-display",
-	  		data: $('form#mela-auction').serialize(),
-	  		success: function() {
-	  			console.log('saved to db');
-	  		}
-	  	});
-	  	var total = Object.keys(names).length
-	  	socket.emit('chat message', item.repeat(total));
-	  	socket.emit('names message', names);
-	  	$('#mela-item').val('AUCTION ITEM');
-	  	$('#guest-list').val('');
-	  	$('#success').show();
-	  	$('#error').hide();
-	  	$("html, body").animate({ scrollTop: 0 }, 200);
+			if (item === 'Auction Item') {
+				$('#error').show();
+				$("html, body").animate({ scrollTop: 0 }, 200);
+			} else {
+	  		$.ajax({
+		  		type: "POST",
+		  		url: "/add-to-display",
+		  		data: $('form#mela-auction').serialize(),
+		  		success: function() {
+		  			console.log('saved to db');
+		  		}
+		  	});
+		  	var total = Object.keys(names).length
+		  	socket.emit('chat message', item.repeat(total));
+		  	socket.emit('names message', names);
+		  	$('#mela-item').val('AUCTION ITEM');
+		  	$('#guest-list').val('');
+		  	$('#success').show();
+		  	$('#error').hide();
+		  	$("html, body").animate({ scrollTop: 0 }, 200);
+		  };
   	} else {
   		$('#success').hide();
   		$('#error').show();
